fc03eb79d4ebc4d670da31e78485d133dcfea85f
David Brownstone <davidbrownstone`2me.com>
2017-08-11 13:59:07 +0300

    added chat settings (partial)

---
+++ NiaNow/ChatLimits.swift
@@ -0,0 +1,42 @@
+//
+//  ChatLimits.swift
+//  NiaNow
+//
+//  Created by David Brownstone on 11/08/2017.
+//  Copyright © 2017 David Brownstone. All rights reserved.
+//
+
+import UIKit
+
+class ChatLimits: UIViewController {
+
+    var modeClass = true
+    var returnToSettings = "returnToSettings"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func setAction(_ sender: Any) {
+    }
+
+    @IBAction func cancelAction(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NiaNow/Settings/Settings.swift
+++ NiaNow/Settings/Settings.swift
@@ -30,29 +30,12 @@ class Settings: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
-    override func tableView(_ tableView : UITableView, viewForHeaderInSection section: Int)->UIView {
-        let headerLabel = UILabel()
-        if section == 0 {
-            headerLabel.text = "Chat Message Availability"
-            headerLabel.backgroundColor = UIColor.lightGray
-        }
-        
-        return headerLabel
-    }
     
-    
-    func tableView (tableView:UITableView , heightForHeaderInSection section:Int)->Float
-    {
-        
-        return 20.0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 2
     }
 
@@ -72,50 +55,34 @@ class Settings: UITableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @IBAction func unwindToSettings(segue:UIStoryboardSegue) {
+        if segue.identifier == "unwindSegueId" {
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0:
+                self.performSegue(withIdentifier: "showChatConversationLimit", sender: self)
+            default:
+                self.performSegue(withIdentifier: "showSinglesConversationLimit", sender: self)
+            }
+        default:
+            return
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let controller = segue.destination
+        if segue.identifier == "showChatConversationLimit" {
+            (controller as! ChatLimits).modeClass = true
+        } else if segue.identifier == "showSinglesConversationLimit" {
+            (controller as! ChatLimits).modeClass = false
+        }
     }
-    */
 
 }
