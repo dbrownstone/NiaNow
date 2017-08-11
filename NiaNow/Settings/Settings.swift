9b8d789d2de3eec3d02deb232c405bba5c71c273
david <davidbrownstone@me.com>
2017-08-11 07:13:16 +0300

    added Settings

---
+++ NiaNow/Settings/Settings.swift
@@ -0,0 +1,121 @@
+//
+//  Settings.swift
+//  NiaNow
+//
+//  Created by David Brownstone on 10/08/2017.
+//  Copyright © 2017 David Brownstone. All rights reserved.
+//
+
+import UIKit
+
+class Settings: UITableViewController {
+
+    var conversationLimitSegue = "showConversationLimit"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView : UITableView, viewForHeaderInSection section: Int)->UIView {
+        let headerLabel = UILabel()
+        if section == 0 {
+            headerLabel.text = "Chat Message Availability"
+            headerLabel.backgroundColor = UIColor.lightGray
+        }
+        
+        return headerLabel
+    }
+    
+    
+    func tableView (tableView:UITableView , heightForHeaderInSection section:Int)->Float
+    {
+        
+        return 20.0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style:.subtitle, reuseIdentifier: "subtitle")
+
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Class Messages"
+            cell.detailTextLabel?.text = "30 days"
+        default:
+            cell.textLabel?.text = "One-on-one Messages"
+            cell.detailTextLabel?.text = "7 days"
+        }
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
