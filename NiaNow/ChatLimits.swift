ea31c821f3294dbcce3917a6fca8063a40b2aee9
david <davidbrownstone@me.com>
2017-08-11 14:50:07 +0300

    chat settings continued

--- NiaNow/ChatLimits.swift
+++ NiaNow/ChatLimits.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
-class ChatLimits: UIViewController {
+class ChatLimits: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     var modeClass = true
     var returnToSettings = "returnToSettings"
+    let classChoices = ["One Day", "Seven Days", "30 Days", "6 Months", "1 Year", "Never"]
+    let classChoicesDictionary = ["One Day": 1, "Seven Days": 7, "30 Days": 30, "6 Months": 183, "1 Year": 366, "Never" : -1]
+    let individualChoices = ["One Day", "Seven Days", "30 Days"]
+    let individualChoicesDictionary = ["One Day": 1, "Seven Days": 7, "30 Days": 30]
+    
+    var selectedEntry:String?
+    @IBOutlet weak var picker: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,8 +36,36 @@ class ChatLimits: UIViewController {
     }
 
     @IBAction func cancelAction(_ sender: Any) {
+        self.performSegue(withIdentifier: "returnToSettings", sender: self)
     }
-    /*
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if modeClass {
+            return self.classChoices.count
+        }
+        return self.individualChoices.count
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if modeClass {
+            return self.classChoices[row]
+        }
+        return self.individualChoices[row]
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if modeClass {
+            selectedEntry = self.classChoices[row]
+        } else {
+            selectedEntry = self.individualChoices[row]
+        }
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -37,6 +73,6 @@ class ChatLimits: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
